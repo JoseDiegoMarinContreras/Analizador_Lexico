1868eda540fa383fe3906f82ac704f8853328dac
agustinbarajas <abv1703@hotmail.com>
2017-04-19 10:37:27 -0600

    Commit Analizador Lexico.

---
+++ src/Simbolo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author abv17
+ */
+public class Simbolo {
+    protected Object nombre;
+    protected Object tipo;
+    protected Object valor;
+    protected Object fila;
+    Simbolo(Object nom, Object tip, Object val){
+        nombre=nom;
+        tipo=tip;
+        valor=val;
+    }
+}
